562c873404bd2912798de06d84d3be4b855c824d
Ayush <Ayush Raghuwanshi>
2021-01-18 20:06:47 +0530

    create endpoints

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.get('/api/branches/autocomplete', (req, res, next) => {
+    res.send(req.query.x);
+});
+
+app.get('/api/branches', (req, res, next) => {
+    res.send("hoho");
+});
+
+app.listen(port, () => {
+    console.log("server is listing on port http://localhost:3000");
+});
