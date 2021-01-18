270af94f43112fc9c2fd01211e436296e75b1623
Ayush <Ayush Raghuwanshi>
2021-01-19 05:21:50 +0530

    co-origin fix for heroku react app

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const port = process.env.PORT || 8080;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://react-hosted.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     // Request methods you wish to allow
