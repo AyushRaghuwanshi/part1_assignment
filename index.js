f6ead0660062c23ed0e5bc66b2b7308706e9a72e
Ayush <Ayush Raghuwanshi>
2021-01-19 02:04:26 +0530

    solve no-core problem

--- index.js
+++ index.js
@@ -16,6 +16,27 @@ client.connect();
 
 const port = process.env.PORT || 8080;
 
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 app.get('/api/branches/autocomplete', (req, res, next) => {
     const query = `
     select * from branches where branch ilike '${req.query.q}%' order by ifsc limit ${req.query.limit} offset ${req.query.offset};
@@ -29,6 +50,7 @@ app.get('/api/branches/autocomplete', (req, res, next) => {
         }
     });
 });
+
 
 
 app.get('/api/branches', (req, res, next) => {
@@ -46,6 +68,7 @@ app.get('/api/branches', (req, res, next) => {
         }
     });
 });
+
 
 
 
